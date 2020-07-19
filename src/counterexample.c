3d44d8fdea85c1591d1c9ee894b8e65d2c8a3a90
Akim Demaille <akim.demaille@gmail.com>
2020-07-19 19:46:41 +0200

    cex: don't issue an empty line between counterexamples
    Now that we use complain, the "sections" are clearer.
    
    * src/counterexample.c (print_counterexample): Use the empty line only
    in reports.
    * tests/counterexample.at, tests/diagnostics.at, tests/report.at: Adjust.

--- src/counterexample.c
+++ src/counterexample.c
@@ -136,7 +136,8 @@ print_counterexample (const counterexample *cex, FILE *out, const char *prefix)
            prefix, cex->shift_reduce ? _("Reduce derivation") : _("Second derivation"));
   derivation_print (cex->d2, out, prefix);
 
-  fputc ('\n', out);
+  if (out != stderr)
+    putc ('\n', out);
 }
 
 /*
@@ -1259,7 +1260,7 @@ counterexample_report_shift_reduce (state_item_number itm1, state_item_number it
       fputc ('\n', out);
     }
   // In the report, print the items.
-  if (*prefix || trace_flag & trace_cex)
+  if (out != stderr || trace_flag & trace_cex)
     {
       print_state_item (&state_items[itm1], out, prefix);
       print_state_item (&state_items[itm2], out, prefix);
@@ -1295,7 +1296,7 @@ counterexample_report_reduce_reduce (state_item_number itm1, state_item_number i
     obstack_free (&obstack, NULL);
   }
   // In the report, print the items.
-  if (*prefix || trace_flag & trace_cex)
+  if (out != stderr || trace_flag & trace_cex)
     {
       print_state_item (&state_items[itm1], out, prefix);
       print_state_item (&state_items[itm2], out, prefix);
