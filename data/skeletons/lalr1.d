a042be869e2177d8fe35d8b4ea3fe69606f7ef2d
Adela Vais <adela.vais@yahoo.com>
2020-11-01 22:13:42 +0200

    d: fix Context class
    All methods are now declared as const. Method getExpectedTokens() has now the
    functionality of reporting only the number of expected tokens.
    
    * data/skeletons/lalr1.d: Fix Context class.

--- data/skeletons/lalr1.d
+++ data/skeletons/lalr1.d
@@ -721,9 +721,9 @@ m4_popdef([b4_at_dollar])])dnl
   public static final class Context
   {
 
-    private YYStack yystack;
+    private const(YYStack) yystack;
     private SymbolKind yytoken;]b4_locations_if([[
-    private ]b4_location_type[ yylocation;]])[
+    private const(]b4_location_type[) yylocation;]])[
 
     this(YYStack stack, SymbolKind kind]b4_locations_if([[, ]b4_location_type[ loc]])[)
     {
@@ -737,7 +737,7 @@ m4_popdef([b4_at_dollar])])dnl
       return yytoken;
     }]b4_locations_if([[
 
-    final ]b4_location_type[ getLocation()
+    final const(]b4_location_type[) getLocation() const
     {
       return yylocation;
     }]])[
@@ -747,12 +747,12 @@ m4_popdef([b4_at_dollar])])dnl
      * YYARG is null, return the number of expected tokens (guaranteed to
      * be less than YYNTOKENS).
      */
-    int getExpectedTokens(SymbolKind[] yyarg, int yyargn)
+    int getExpectedTokens(SymbolKind[] yyarg, int yyargn) const
     {
       return getExpectedTokens(yyarg, 0, yyargn);
     }
 
-    int getExpectedTokens(SymbolKind[] yyarg, int yyoffset, int yyargn)
+    int getExpectedTokens(SymbolKind[] yyarg, int yyoffset, int yyargn) const
     {
       int yycount = yyoffset;
       int yyn = yypact_[this.yystack.stateAt(0)];
@@ -769,16 +769,17 @@ m4_popdef([b4_at_dollar])])dnl
         for (int yyx = yyxbegin; yyx < yyxend; ++yyx)
           if (yycheck_[yyx + yyn] == yyx && yyx != ]b4_symbol(1, kind)[
               && !yyTableValueIsError(yytable_[yyx + yyn]))
-            yycount++;
-        if (yycount < yyargn)
-        {
-          yycount = 0;
-          for (int x = yyxbegin; x < yyxend; ++x)
-            if (yycheck_[x + yyn] == x && x != ]b4_symbol(1, kind)[
-                && !yyTableValueIsError(yytable_[x + yyn]))
-              yyarg[yycount++] = SymbolKind(x);
-        }
+          {
+            if (yyarg is null)
+              ++yycount;
+            else if (yycount == yyargn)
+              return 0;
+            else
+              yyarg[yycount++] = SymbolKind(yyx);
+          }
       }
+      if (yyarg !is null && yycount == 0 && 0 < yyargn)
+        yyarg[0] = ]b4_symbol(empty, kind)[;
       return yycount - yyoffset;
     }
   }
@@ -897,7 +898,7 @@ m4_popdef([b4_at_dollar])])dnl
       stack.length -= num;
     }
 
-    public final int stateAt (int i)
+    public final int stateAt (int i) const
     {
       return stack[$-i-1].state;
     }
